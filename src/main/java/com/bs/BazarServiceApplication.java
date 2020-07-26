7652f34df3b1485e520a7bfcedbcc17c176cbb5b
Shaheen <shaheen_equbal@thbs.com>
2020-07-26 21:51:57 +0530

    first phase development hav don

---
+++ src/main/java/com/bs/BazarServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazarServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazarServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bs/user/dto/User.java
@@ -0,0 +1,38 @@
+package com.bs.user.dto;
+
+public class User {
+	
+	private String name;
+	private String contactNo;
+	private String address;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContactNo() {
+		return contactNo;
+	}
+
+	public void setContactNo(String contactNo) {
+		this.contactNo = contactNo;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", contactNo=" + contactNo + ", address=" + address + "]";
+	}
+
+}
